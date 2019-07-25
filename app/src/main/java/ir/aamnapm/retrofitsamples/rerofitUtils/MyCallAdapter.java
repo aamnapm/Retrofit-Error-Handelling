89b6bb601d246378497468fd6a2ab5b59f9c6276
akbari <akbari@bdood.ir>
2019-07-25 14:22:48 +0430

    add callbackExecutor on MyCallAdapter class

--- app/src/main/java/ir/aamnapm/retrofitsamples/rerofitUtils/MyCallAdapter.java
+++ app/src/main/java/ir/aamnapm/retrofitsamples/rerofitUtils/MyCallAdapter.java
@@ -31,17 +31,20 @@ public class MyCallAdapter<T> implements MyCall<T> {
                 // on that executor by submitting a Runnable. This is left as an exercise for the reader.
 
                 int code = response.code();
-                if (code >= 200 && code < 300) {
-                    callback.success(response);
-                } else if (code == 401) {
-                    callback.unauthenticated(response);
-                } else if (code >= 400 && code < 500) {
-                    callback.clientError(response);
-                } else if (code >= 500 && code < 600) {
-                    callback.serverError(response);
-                } else {
-                    callback.unexpectedError(new RuntimeException("Unexpected response " + response));
-                }
+                callbackExecutor.execute(() -> {
+
+                    if (code >= 200 && code < 300) {
+                        callback.success(response);
+                    } else if (code == 401) {
+                        callback.unauthenticated(response);
+                    } else if (code >= 400 && code < 500) {
+                        callback.clientError(response);
+                    } else if (code >= 500 && code < 600) {
+                        callback.serverError(response);
+                    } else {
+                        callback.unexpectedError(new RuntimeException("Unexpected response " + response));
+                    }
+                });
             }
 
             @Override
